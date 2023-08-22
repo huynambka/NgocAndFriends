522938dd00fc4aaff7d6183ddfdbafd8d8c5f9e1
huynambka <huynambka@gmail.com>
2023-08-22 22:57:15 +0700

    update user controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,29 +1,41 @@
-// TODO: Authorization
-
 const User = require('../models/User');
 
-const getAllUsers = async (req, res) => {
-    // TODO: Pagination
-    const users = await User.find({});
-    if (!users) return next(new Error('No users found'));
-    res.status(200).json({ count: users.length, users: users });
-};
+// const getAllUsers = async (req, res) => {
+//     // TODO: Pagination
+//     const users = await User.find({});
+//     if (!users) return next(new Error('No users found'));
+//     res.status(200).json({ count: users.length, users: users });
+// };
 
 const getUserInfo = async (req, res, next) => {
     const username = req.body.username;
+    const userId = req.user.id;
     const user = await User.findOne({ username });
     if (!user) next(new Error('User not found'));
-    res.status(200).json({ user });
+    user.password = undefined;
+    if (user._id.toString() !== userId.toString() && req.user.role !== 'admin') {
+        res.status(200).json({ username: user.username, name: user.name, social: user.social });
+    } else {
+        res.status(200).json({ user });
+    }
 };
-const updateUserInfo = async (req, res) => {
-    const updatedUser = { ...req.body };
-    const user = await User.findOneAndUpdate({ username: req.body.username }, updatedUser, { new: true });
+const updateUserInfo = async (req, res, next) => {
+    const bodyUser = { ...req.body };
+    if (bodyUser.password) {
+        return next(new Error('You are not allowed to edit password'));
+    }
+    const userId = req.user.id;
+    const user = await User.findOne({ username: bodyUser.username });
     if (!user) return next(new Error('User not found'));
-    res.status(200).json({ user });
+    if (user._id.toString() !== userId.toString() && req.user.role !== 'admin') {
+        return next(new Error('You are not allowed to edit this user'));
+    }
+    const updatedUser = await User.findOneAndUpdate({}, bodyUser, { new: true });
+    res.status(200).json({ updatedUser });
 };
 
 module.exports = {
-    getAllUsers,
+    // getAllUsers,
     getUserInfo,
     updateUserInfo,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userControllers = require('../controllers/user.controller');
 
-router.get('/all', userControllers.getAllUsers);
-router.get('/getUserInfo', userControllers.getUserInfo);
-router.put('/updateUserInfo', userControllers.updateUserInfo);
+const passport = require('../middlewares/passport');
+const passportJWT = passport.authenticate('jwt', { session: false }, null);
+
+router.get('/getUserInfo', passportJWT, userControllers.getUserInfo);
+router.put('/updateUserInfo', passportJWT, userControllers.updateUserInfo);
 router.get('/', (req, res) => {
     res.send('Hello User!');
 });
