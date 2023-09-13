5eb04a13a2f04cc86fbafa13383c2415ac4f483b
huynambka <huynambka@gmail.com>
2023-09-14 03:33:09 +0700

    add cors

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ require('dotenv').config();
 const express = require('express');
 const { Server } = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+    }),
+);
 
 const io = new Server(server, {
     cors: {
